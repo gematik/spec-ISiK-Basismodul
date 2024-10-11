cc6d0dead45f91c638ae7816fe0459c086eafea3
f-peverali <112709306+f-peverali@users.noreply.github.com>
2024-10-11 15:52:43 +0200

    update test as not modifying index.html

--- scripts/test_update_index.py
+++ scripts/test_update_index.py
@@ -1,11 +1,9 @@
 import unittest
 from unittest.mock import mock_open, patch
 import datetime
-from update_index import create_new_row, update_index_html, validate_version
+from update_index import create_new_row, update_index_html, validate_version, read_index_html, find_insert_position
 
 class TestUpdateIndex(unittest.TestCase):
-
-#TODO currently thes modify index.html file, so we need to mock the file content and test the functions 
 
     def test_update_index_with_redundant_version(self):
         """
@@ -19,7 +17,25 @@ class TestUpdateIndex(unittest.TestCase):
             The function prints an error message and exits with status code 1.
         """
         version = "3.0.5"
-        with patch("sys.exit") as mock_exit:
+        mock_file_content = """
+        <tr>
+        <td>01.01.2022</td> 
+        <td>
+            <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.5/ImplementationGuide-markdown-Einfuehrung.html">3.0.5</a>
+        </td>
+        <td>Technical Correction 3.0.5</td>
+        <td>3.0.5</td>
+        </tr>
+        <tr>
+        <td>01.01.2022</td> 
+        <td>
+            <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.4/ImplementationGuide-markdown-Einfuehrung.html">3.0.4</a>
+        </td>
+        <td>Technical Correction 3.0.4</td>
+        <td>3.0.4</td>
+        </tr>
+        """
+        with patch("builtins.open", mock_open(read_data=mock_file_content)), patch("sys.exit") as mock_exit:
             update_index_html(version)
             mock_exit.assert_called_once_with(1)
 
@@ -89,42 +105,64 @@ class TestUpdateIndex(unittest.TestCase):
             mock_exit.assert_called_once_with(1)
 
 
+    # TODO fix test
+    '''def test_update_index_html_insertion_position(self):
+        """
+        Test case for the `update_index_html` function.
 
-    # TODO def test_update_index_html_insertion_position(self,version="3.0.6"):
-        """ 
-        One Test case for the `update_index_html` function. 
         This test verifies that the `update_index_html` function correctly determines the insertion position for the new version in the index.html file.
+
         The test uses a mock file content with a specific version pattern to test the insertion position.
-        The expected insertion position is above the row with the previous version number that is less than the new version number. 
 
-        Asserts: 
-            The insertion position is correctly determined based on the previous version number.
-        """
-
-
-
-    # TODO ? def test_create_new_row(self):
-        """
-        Test case for the `create_new_row` function.
-
-        This test verifies that the `create_new_row` function correctly generates
-        an HTML table row with the given version and current date.
-
-        The expected HTML row includes:
-        - The current date in the first cell.
-        - A hyperlink to the Implementation Guide for the given version in the second cell.
-        - A description "Technical Correction" followed by the version in the third cell.
-        - The version in the fourth cell.
-
-        The test compares the generated row with the expected row to ensure they match.
-
-        Args:
-            self: The test case instance.
+        The expected insertion position is above the row with the previous version number that is less than the new version number.
 
         Asserts:
-            The generated HTML row matches the expected HTML row.
+            The insertion position is correctly determined based on the previous version number.
         """
-        # TODO
+        version = "3.0.6"
+        mock_file_content = """
+        <tr>
+            <td>04.04.2024</td>
+            <td>
+                <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/4.0.0-rc2/ImplementationGuide-markdown-Einfuehrung.html">4.0.0-rc2</a>
+            </td>
+            <td>Release Candidate Kommentierung 4.0.0-rc2</td>
+            <td>4.0.0-rc2</td>
+            </tr>
+            <tr>
+        </tr>
+            <td colspan="4" style="background-color: #dddddd;">
+                <strong>Basismodul Stufe 3</strong></td>
+        </tr>
+        <tr>
+            <tr>
+            <td>30.04.2024</td>
+            <td>
+                <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.5/ImplementationGuide-markdown-Einfuehrung.html">3.0.5</a>
+            </td>
+            <td>Technical Correction 3.0.5</td>
+            <td>3.0.5</td>
+            </tr>
+            <tr>
+        <tr>
+            <tr>
+            <td>03.04.2024</td>
+            <td>
+                <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.4/ImplementationGuide-markdown-Einfuehrung.html">3.0.4</a>
+            </td>
+            <td>Technical Correction 3.0.4</td>
+            <td>3.0.4</td>
+            </tr>
+            <tr>
+        """
+        expected_position = mock_file_content.find('<tr>\n<tr>\n<td>30.04.2024</td><tr>\n<td>01.01.2022</td> \n<td>\n<a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.5/ImplementationGuide-markdown-Einfuehrung.html">3.0.5</a>')
+
+        with patch("builtins.open", mock_open(read_data=mock_file_content)), patch("sys.exit") as mock_exit:
+            content = read_index_html()
+            insert_position = find_insert_position(content, version)
+            self.assertEqual(insert_position, expected_position)
+            mock_exit.assert_not_called()
+    '''
   
 
 if __name__ == "__main__":
