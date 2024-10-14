600a3c55101155071c6b43f017368ab268f47a17
f-peverali <112709306+f-peverali@users.noreply.github.com>
2024-10-14 17:59:27 +0200

    update tests

--- scripts/test_update_index.py
+++ scripts/test_update_index.py
@@ -1,20 +1,20 @@
 import unittest
 from unittest.mock import mock_open, patch
 import datetime
-from update_index import create_new_row, update_index_html, validate_version, read_index_html, find_insert_position
+from update_index import create_new_row, get_current_date_str, update_content, update_index_html, validate_version, read_index_html, find_insert_position
 
 class TestUpdateIndex(unittest.TestCase):
 
     def test_update_index_with_redundant_version(self):
         """
-        Test case for the `update_index_html` function.
+        Test case for the `validate_version` function.
 
-        This test verifies that the `update_index_html` function correctly handles a redundant version.
+        This test verifies that the `validate_version` function correctly handles a redundant version.
 
         The test uses a version number that is equal to an existing version number.
 
         Asserts:
-            The function prints an error message and exits with status code 1.
+            The function prints an error message and returns with status false.
         """
         version = "3.0.5"
         mock_file_content = """
@@ -35,9 +35,11 @@ class TestUpdateIndex(unittest.TestCase):
         <td>3.0.4</td>
         </tr>
         """
-        with patch("builtins.open", mock_open(read_data=mock_file_content)), patch("sys.exit") as mock_exit:
-            update_index_html(version)
-            mock_exit.assert_called_once_with(1)
+        validation_result = validate_version(version, mock_file_content)
+        self.assertEqual(validation_result, (False, f"Version {version} already exists in index.html."))
+
+        
+
 
     def test_validate_version_with_incorrect_format(self, version="4.0", content=""):
         """
@@ -67,8 +69,8 @@ class TestUpdateIndex(unittest.TestCase):
         """
         with patch("sys.exit") as mock_exit:
             version = "4.0.11"
-            validate_version(version,content="")
-            mock_exit.assert_not_called()
+            validation_result = validate_version(version,content="")
+            self.assertEqual(validation_result, (True, "Version has no obvious issues."))
 
     def test_validate_version_with_redundant_version(self):
         """
@@ -100,69 +102,118 @@ class TestUpdateIndex(unittest.TestCase):
         <td>3.0.4</td>
         </tr>
         """
-        with patch("sys.exit") as mock_exit:
-            validate_version(version, content)
-            mock_exit.assert_called_once_with(1)
+        validation_result = validate_version(version, content)
+        self.assertEqual(validation_result, (False, f"Version {version} already exists in index.html."))    
 
-
-    # TODO fix test
-    def test_update_index_html_insertion_position(self):
+    def test_find_insert_position_with_no_previous_version(self):
         """
-        Test case for the `update_index_html` function.
+        Test case for the `find_insert_position` function.
 
-        This test verifies that the `update_index_html` function correctly determines the insertion position for the new version in the index.html file.
+        This test verifies that the `find_insert_position` function correctly handles the case when no previous version is found in the index.html file.
 
-        The test uses a mock file content with a specific version pattern to test the insertion position.
-
-        The expected insertion position is above the row with the previous version number that is less than the new version number.
+        The test uses a mock file content with no previous version.
 
         Asserts:
-            The insertion position is correctly determined based on the previous version number.
+            The function prints an error message and exits with status code 1.
         """
         version = "3.0.6"
-        mock_file_content = """
-        <tr>
-            <td>04.04.2024</td>
-            <td>
-                <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/4.0.0-rc2/ImplementationGuide-markdown-Einfuehrung.html">4.0.0-rc2</a>
-            </td>
-            <td>Release Candidate Kommentierung 4.0.0-rc2</td>
-            <td>4.0.0-rc2</td>
-            </tr>
-            <tr>
-        </tr>
-            <td colspan="4" style="background-color: #dddddd;">
-                <strong>Basismodul Stufe 3</strong></td>
-        </tr>
-        <tr>
-            <tr>
-            <td>30.04.2024</td>
-            <td>
-                <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.5/ImplementationGuide-markdown-Einfuehrung.html">3.0.5</a>
-            </td>
-            <td>Technical Correction 3.0.5</td>
-            <td>3.0.5</td>
-            </tr>
-            <tr>
-        <tr>
-            <tr>
-            <td>03.04.2024</td>
-            <td>
-                <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.4/ImplementationGuide-markdown-Einfuehrung.html">3.0.4</a>
-            </td>
-            <td>Technical Correction 3.0.4</td>
-            <td>3.0.4</td>
-            </tr>
-            <tr>
-        """
-        expected_position = mock_file_content.find('<tr>\n<tr>\n<td>30.04.2024</td><tr>\n<td>01.01.2022</td> \n<td>\n<a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.5/ImplementationGuide-markdown-Einfuehrung.html">3.0.5</a>')
+        mock_file_content = ""
+        position_result = find_insert_position(mock_file_content, version)
+        self.assertEqual(position_result, (False, "No previous version found in index.html"))
 
-        with patch("builtins.open", mock_open(read_data=mock_file_content)), patch("sys.exit") as mock_exit:
-            content = mock_file_content
-            insert_position = find_insert_position(content, version)
-            self.assertEqual(insert_position, expected_position)
-            mock_exit.assert_not_called()
-    
+
+    def test_find_insert_position_with_correct_previous_version_given_front_position_random_string_of_len_10(self):
+        """
+        Test case for the `find_insert_position` function.
+
+        This test verifies that the `find_insert_position` function correctly finds the insert position when a correct previous version is given.
+
+        The test uses a mock file content with a previous version.
+
+        Asserts:
+            The function returns the correct insert position.
+        """
+        version = "3.0.6"
+        mock_file_content = """1234567890<tr>
+        <td>01.01.2022</td> 
+        <td>
+            <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.5/ImplementationGuide-markdown-Einfuehrung.html">3.0.5</a>
+        </td>
+        <td>Technical Correction 3.0.5</td>
+        <td>3.0.5</td>
+        </tr>
+        <tr>
+        <td>01.01.2022</td> 
+        <td>
+            <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.4/ImplementationGuide-markdown-Einfuehrung.html">3.0.4</a>
+        </td>
+        <td>Technical Correction 3.0.4</td>
+        <td>3.0.4</td>
+        </tr>
+        """
+        position_result, message = find_insert_position(mock_file_content, version)
+        self.assertEqual(position_result, 10)
+
+    def test_find_insert_position_with_correct_previous_version_given_in_between_position(self):
+        """
+        Test case for the `find_insert_position` function.
+
+        This test verifies that the `find_insert_position` function correctly finds the insert position when a correct previous version is given.
+
+        The test uses a mock file content with a previous version.
+
+        Asserts:
+            The function returns the correct insert position.
+        """
+        version = "3.0.6"
+        mock_file_content = """<tbody>
+    </tr>
+        <td colspan="4" style="background-color: #dddddd;">
+            <strong>Basismodul Stufe 4</strong></td>
+    </tr>
+    <tr>
+        <tr>
+        <td>25.06.2024</td>
+        <td>
+            <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/4.0.0-rc3/ImplementationGuide-markdown-Einfuehrung.html">4.0.0-rc3</a>
+        </td>
+        <td>Release Candidate Benehmensherstellung 4.0.0-rc3</td>
+        <td>4.0.0-rc3</td>
+        </tr>
+        <tr>
+        <td>04.04.2024</td>
+        <td>
+            <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/4.0.0-rc2/ImplementationGuide-markdown-Einfuehrung.html">4.0.0-rc2</a>
+        </td>
+        <td>Release Candidate Kommentierung 4.0.0-rc2</td>
+        <td>4.0.0-rc2</td>
+        </tr>
+        <tr>
+    </tr>
+        <td colspan="4" style="background-color: #dddddd;">
+            <strong>Basismodul Stufe 3</strong></td>
+    </tr>
+    <tr>
+        <tr>
+        <td>30.04.2024</td>
+        <td>
+          <a href="https://gematik.github.io/spec-ISiK-Basismodul/IG/3.0.5/ImplementationGuide-markdown-Einfuehrung.html">3.0.5</a>
+        </td>
+        <td>Technical Correction 3.0.5</td>
+        <td>3.0.5</td>
+      </tr>
+      <tr>
+    <tr>"""
+        position_result, message = find_insert_position(mock_file_content, version)
+        self.assertEqual(position_result, 968 ) 
+        # 968 is the length before "<tr>
+        # <td>30.04.2024</td> [...]", which is date for 3.0.5 element.'''
+        #TODO not working correclty - fix issue in code
+
+
+
+        
+
   
 
 if __name__ == "__main__":
