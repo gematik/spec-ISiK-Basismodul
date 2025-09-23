001aa374eb1208249a41b5d0f4400e50fd790cf3
f-peverali <112709306+f-peverali@users.noreply.github.com>
2025-09-23 13:58:35 +0200

    update config and scripts for gCloud deployment

---
+++ publisher-guides/Subscription-IG/config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+TARGET=5.0.0
+BUCKET_PATH=/ig/fhir/isik-basismodul
+PREV="" # Leave empty to skip moving
+BUCKET_NAME_DEV=gematik_gemspec_fhir_dev-0
+BUCKET_NAME_PROD=gematik_gemspec_fhir_prod-0
