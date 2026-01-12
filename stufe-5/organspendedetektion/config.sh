2ecfe8ad638a25e6a700eded31642036c6337d15
f-peverali <112709306+f-peverali@users.noreply.github.com>
2026-01-12 15:53:45 +0100

    add decoker file

---
+++ stufe-5/organspendedetektion/config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+TARGET=0.0.1-rc
+BUCKET_PATH=/ig/fhir/isik/Organspendeerkennung
+PREV="" # Leave empty to skip moving
+BUCKET_NAME_DEV=gematik_gemspec_fhir_dev-0
+BUCKET_NAME_PROD=gematik_gemspec_fhir_prod-0
