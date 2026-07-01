d0c805fefb6694a673e1377e5ecf819c12d0513b
f-peverali <112709306+f-peverali@users.noreply.github.com>
2026-07-01 15:56:59 +0200

    prod html redirects fetched

--- scripts/link-update-latest/helper/download-current-latest-tree.sh
+++ scripts/link-update-latest/helper/download-current-latest-tree.sh
@@ -4,6 +4,11 @@
 # Prerequisites: gcloud auth login, gsutil (Google Cloud SDK), bash (Linux/macOS/WSL)
 
 # download all latest folders including html (/isik/basis/latest + /isik/vitalparameter/latest etc.) from dev 
-gsutil -m cp -r "gs://gematik_gemspec_fhir_dev-0/ig/fhir/isik/*/latest/" ./latest-download/
+mkdir -p ./latest-download-dev
+gsutil -m cp -r "gs://gematik_gemspec_fhir_dev-0/ig/fhir/isik/*/latest/" ./latest-download-dev/
+
+# download all latest folders including html (/isik/basis/latest + /isik/vitalparameter/latest etc.) from prod 
+mkdir -p ./latest-download-prod
+gsutil -m cp -r "gs://gematik_gemspec_fhir_prod-0/ig/fhir/isik/*/latest/" ./latest-download-prod/
 
 
